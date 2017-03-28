03631c85c0a237e77c8658b1732b072c3109801e
島崎倫也 <acfa2656@chiba-u.jp>
2017-03-28 14:46:35 +0900

    echobot

--- app.rb
+++ app.rb
@@ -1,18 +1,11 @@
-require 'sinatra'
-require 'line/bot'
-require 'rest-client'
+require 'sinatra'   # gem 'sinatra'
+require 'line/bot'  # gem 'line-bot-api'
 
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
-end
-
-def get_user_local_bot_reply(word)
-  response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word) } }
-  response_json = JSON.parse(response)
-  response_json['status'] == "success" ? response_json['result'] : '通信エラー'
 end
 
 post '/callback' do
@@ -24,6 +17,7 @@ post '/callback' do
   end
 
   events = client.parse_events_from(body)
+
   events.each { |event|
     case event
     when Line::Bot::Event::Message
@@ -31,13 +25,9 @@ post '/callback' do
       when Line::Bot::Event::MessageType::Text
         message = {
           type: 'text',
-          text: get_user_local_bot_reply(event.message['text'])
+          text: event.message['text']
         }
         client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
       end
     end
   }
